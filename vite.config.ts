a890054ef7b775a310fdad72595949dd779938d9
SantiQ <a01571222@itesm.mx>
2026-04-24 17:06:14 -0600

    Updates for testing

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,17 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/health': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
+  host: '0.0.0.0',
+  proxy: {
+    '/api': {
+      target: 'http://localhost:8080',
+      changeOrigin: true,
     },
+    '/health': {
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+    },
+  },
   },
   test: {
     environment: 'jsdom',
